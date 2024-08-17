67e1cae7d65aeca522dde0cbfbf0bc2f7af635ad
Uttam_02 <mahatauttam0408@gmail.com>
2024-08-17 17:01:42 +0530

    chore: Update project dependencies and configurations

---
+++ src/test/java/com/winners/scoreboard/ScoreboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.winners.scoreboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoreboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
