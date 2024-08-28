962c5551e1b670ca62b61c3a3dd619513cb1c5b6
Uttam_02 <mahatauttam0408@gmail.com>
2024-08-28 18:08:45 +0530

    chore: Update CrossOrigin origins in ScoreboardApplication.java and WebConfig.java

--- src/main/java/com/winners/scoreboard/config/StaticConfig.java
+++ src/main/java/com/winners/scoreboard/config/StaticConfig.java
@@ -1,4 +1,27 @@
 package com.winners.scoreboard.config;
 
-public class StaticConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class StaticConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/", "file:index.html");
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/{spring:\\w+}")
+                .setViewName("forward:/index.html");
+        registry.addViewController("/**/{spring:\\w+}")
+                .setViewName("forward:/index.html");
+        registry.addViewController("/{spring:\\w+}/**{spring:?!(\\.js|\\.css)$}")
+                .setViewName("forward:/index.html");
+    }
 }
+
