a5802496a45c1f841eb78ed5a955f29e297b029f
Uttam_02 <mahatauttam0408@gmail.com>
2024-08-29 14:03:20 +0530

    chore: Update CrossOrigin origins in ScoreboardApplication and WebConfig

--- src/main/java/com/winners/scoreboard/config/WebConfig.java
+++ src/main/java/com/winners/scoreboard/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200", "https://wscoreboard.web.app/")
+                .allowedOrigins("http://localhost:4200", "https://wscoreboard.web.app/", "https://scoreboard-latest.onrender.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
