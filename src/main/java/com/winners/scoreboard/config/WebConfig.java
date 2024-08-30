6fc34320ebb6076c45c425dae5c36bdd704f2f22
Uttam_02 <mahatauttam0408@gmail.com>
2024-08-30 07:41:55 +0530

    chore: Update CORS allowed origins to include http://localhost:8080

--- src/main/java/com/winners/scoreboard/config/WebConfig.java
+++ src/main/java/com/winners/scoreboard/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200", "https://wscoreboard.web.app/", "https://scoreboard-latest.onrender.com")
+                .allowedOrigins("http://localhost:4200", "https://wscoreboard.web.app/", "https://scoreboard-latest.onrender.com", "http://localhost:8080")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
